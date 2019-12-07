bad64fc0d17073797c8bff8c468c20781b3c83e2
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-07 17:14:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,38 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+ 
+row = 0 
+while row < collection.length do 
+  collection[row]
+  item_name = collection[row][:item]
+  
+  if name == item_name
+    item_find = collection[row]  
+  end
+  
+row += 1 
+end 
+p item_find
 end
 
 def consolidate_cart(cart)
+  
+  row = 0 
+  new_cart = []
+ 
+  while row < cart.length do 
+  cart[row][:count] = 1 
+  item_name = cart[row][:item]
+  
+   new_cart << cart[row]
+    
+  row += 1 
+  # binding.pry 
+
+  end
+ 
+   p new_cart
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
